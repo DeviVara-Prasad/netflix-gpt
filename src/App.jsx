d55def22ba00ddbfe49b42149d0f997b6c37f9f9
Devi Vara Prasad Nulu <nuludevivaraprasad@gmail.com>
2024-03-11 21:26:22 +0530

    initialized React Project and Added Tailwind Css.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-3xl text-yellow-400'>Namaste EveryOne</div>
+    </>
+  )
+}
+
+export default App
